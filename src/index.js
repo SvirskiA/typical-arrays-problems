d59093a5f92cd0c496a9d5a74ea8c4d02643b76a
Anton Svirski <svirski89@gmail.com>
2020-02-24 00:15:09 +0300

    init: submit task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (array === undefined || array.length === 0) {
+    return 0;
+  } else {
+    let min = array[0];
+    for (let i = 1; i < array.length; i++) {
+      array[i] < min ? min = array[i] : false;
+    }
+    return min;
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+
+exports.max = function max(array) {
+  if (array === undefined || array.length === 0) {
+    return 0;
+  } else {
+    let max = array[0];
+    for (let i = 1; i < array.length; i++) {
+      array[i] > max ? max = array[i] : false;
+    }
+    return max;
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+
+
+exports.avg = function avg(array) {
+  if (array === undefined || array.length === 0) {
+    return 0;
+  } else {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+    return sum / array.length;
+  }
 }
